8bd9ceaf2cf42a6450cc4bfc84df11530d36c10e
fam2422 <fam2422@gmail.com>
2025-10-01 16:35:11 +0700

    add project

---
+++ src/main/java/project/main/GangBungApplication.java
@@ -0,0 +1,13 @@
+package project.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GangBungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GangBungApplication.class, args);
+	}
+
+}
